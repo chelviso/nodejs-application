ab731b2d0bbfe9348fac371e539be3ce41627c7e
chelviso <111324611+chelviso@users.noreply.github.com>
2022-10-04 16:03:45 -0500

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.87.96.247:9000/',
+  serverUrl: 'http://54.234.158.91:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
